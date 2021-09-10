b4e34e533d0332fe1834043e210f64c229940223
1942282253 <45612543+1942282253@users.noreply.github.com>
2021-09-10 09:29:31 +0800

    Update ConsulMain8006.java

--- cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/ConsulMain8006.java
+++ cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/ConsulMain8006.java
@@ -3,7 +3,7 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+//测试github
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ConsulMain8006 {
